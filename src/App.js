41b5f0c95b87420f8a97aa5f092b5fb82c6be033
Joshua Pawlik <joshuapawlik12@gmail.com>
2022-07-20 13:23:42 -0500

    Use useEffect and local storage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import TodoList from './TodoList'
+import {v4 as uuidv4} from 'uuid'
 
 function App() {
-  const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  const LOCAL_STORAGE_KEY = 'todoApp.todos'
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+
+    console.log("STORED TODOS ================>", storedTodos)
+    if (storedTodos.length) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() =>{
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+
+  }, [todos])
+
+  function handleAddTodo(e){
+    const name = todoNameRef.current.value
+    if (name === '')  return
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+  }
+
   return (
     <>
     <TodoList todos={todos}/>
-    <input type="text" />
-    <button>Add Todo</button>
+    <input ref={todoNameRef} type="text" />
+    <button onClick={handleAddTodo}>Add Todo</button>
     <button>Clear Completed Todo</button>
     <div>0 left to do</div>
     </>
